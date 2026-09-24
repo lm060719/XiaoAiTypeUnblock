677ac7caf28651682117b23f64fc14c274830fd3
lm060719 <limo060719@gmail.com>
2026-09-24 19:26:01 +0800

    版本号加1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.mo.xatype"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "2.0.2"
+        versionCode = 5
+        versionName = "2.1.0"
     }
 
     buildTypes {
